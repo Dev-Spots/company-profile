de50ecd8777f63200ee06c99a056658fc05e7ea8
anandafiqri <anandafiqri1403@gmail.com>
2024-03-16 19:28:03 +0700

    add:metadata techstack list and add loader

--- src/app/(home)/@tech/page.tsx
+++ src/app/(home)/@tech/page.tsx
@@ -1,4 +1,10 @@
 import TechStackList from "@/components/organs/content/techList";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description:
+    "our tech list golang, postgresql, python, astro, redux bootstrap, cassandra, css, express, html, firebase, graphql, heroku, mongodb, mongoose, mysql, nestjs, netlify, nextjs, nodejs rabbitmq, tailwind, typescript, vercel, socketio, react, vue, redis",
+};
 
 export default function TechSection() {
   return <TechStackList />;
