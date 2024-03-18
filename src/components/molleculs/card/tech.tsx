498614a2dad5f1f33bf61a2f6e17fef6c630fd7d
anandafiqri <anandafiqri1403@gmail.com>
2024-03-18 11:31:02 +0700

    add: color to tech color

--- src/components/molleculs/card/tech.tsx
+++ src/components/molleculs/card/tech.tsx
@@ -8,7 +8,7 @@ export default function TechCard({ name, svg }: TechCardProps) {
     <article className="px-2">
       <div className="flex w-full space-x-2 rounded-full px-4 py-2 shadow-[0_3px_10px_rgb(0,0,0,0.15)] dark:shadow-neutral-800">
         <div className="h-6 w-6">{svg}</div>
-        <div className="whitespace-nowrap">{name}</div>
+        <div className="whitespace-nowrap cursor-pointer">{name}</div>
       </div>
     </article>
   );
